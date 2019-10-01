fce413bf467b7b3df717c604680420232b4cc313
itsdanielguerrero <itsdanielguerrero@gmail.com>
2019-09-30 21:46:29 -0400

    New File w/Array of Objects

---
+++ objects-in-arrays.js
@@ -0,0 +1,7 @@
+const carList = [
+    {year: 2014, make: 'Fiat', model: '500L'},
+    {year: 2018, make: 'Honda', model: 'Accord'},
+    {year: 2014, make: 'Toyota', model: 'Camry'}
+]
+
+console.log(carList)
