966ae0d7fcce7189fb2167c73f133086d091876a
itsdanielguerrero <itsdanielguerrero@gmail.com>
2019-09-30 21:11:14 -0400

    Pushing up

---
+++ data-types.js
@@ -0,0 +1,34 @@
+var num = 1
+var string = 'this is a string'
+var array = [1, 2, 3]
+var bool = true
+var meObject = {
+    firstName: 'Daniel',
+    lastName: 'Guerrero',
+    age: 26
+}
+var today = new Date()
+
+console.log('This is an int ' + num)
+console.log('This is a string ' + string)
+console.log('This is an array ' + array)
+console.log('This is a boolean ' + bool)
+console.log('This is an object ' + meObject + ' and here are some properties: first name = ' + meObject.firstName + ' and \n' 
+    + 'last name = ' + meObject.lastName + ' and age = ' + meObject.age)
+console.log('This is a date ' + today)
+
+const listOfPets = [
+    {petType: 'Dog', name: 'Timon'},
+    {petType: 'Dog', name: 'Sparky'},
+    {petType: 'Guinea Pig', name: 'Nibbles'},
+    {petType: 'Cat', name: 'Popcorn'},
+    {petType: 'Fish', name: 'Great Gaspy'}
+]
+
+function sortByPet (table, pet){
+    const {petType} = pet
+    const petOnT
+    if (petOnT = table[pet]){
+        
+    }
+}
