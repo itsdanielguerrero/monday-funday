b9d2919b91dfe5e51fa25db98f6fd6e34cc51f61
itsdanielguerrero <itsdanielguerrero@gmail.com>
2019-10-02 21:23:02 -0400

    Updated w filter, map and reduce manipulating as expected

--- car-map-reduce-filter.js
+++ car-map-reduce-filter.js
@@ -8,29 +8,39 @@ var cars = [
     {make: 'Toyota', model: 'Camry', year: 2016},
     {make: 'Fiat', model: 'Spider', year: 2015},
     {make: 'Honda', model: 'Accord', year: 2016},
-    {make: 'Kia', model: 'Soul', year: 2017}
+    {make: 'Kia', model: 'Soul', year: 2017},
     {make: 'Hummer', model: 'H2', year: 2015},
-    {make: 'Chevy', model: 'Impala', year: 2015},
+    {make: 'Chevy', model: 'Impala', year: 2015}
 ]
 
+//using filter
 var sameYear = cars.filter( (car) => {
     return car.year === 2015
 } )
 
 console.log(sameYear)
 
-var makeModel = cars.map( (car) => {
-    return 'This car is a ' + car.model + ' and it is made by ' + car.make + '.'
+//using map
+var displayData = cars.map( (car) => {
+    return 'This car is a ' + car.model + ' and it is made by ' + car.make + ' in the year ' + car.year + '.'
 } )
 
-console.log(makeModel)
+console.log(displayData)
+
+//using reduce
+function onTable (table, year){
+    if (table[year]){
+        return table[year]
+    }
+    return[]
+}
 
 var byYear = cars.reduce( (table, car) => {
-    const {make, model, year} = cars
-    const currentCar = []
-    if (table[year]){
-        currentCar = table.year
-    }
-    addCar = currentCar.concat(make, model)
+    const {make, model, year} = car
+    const currentCar = onTable(table, year)
+
+    addCar = currentCar.concat(make + "-" + model)
     return {...table, [year]: addCar}
 },{})
+
+console.log (byYear)
